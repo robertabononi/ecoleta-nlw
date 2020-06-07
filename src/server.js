a9b948728959bfa974fb962fba3784e90e6f2494
Roberta Bononi da Silva <robertabononi@users.noreply.github.com>
2020-06-07 04:25:32 -0300

    renderizacao e enviando dados para html

--- src/server.js
+++ src/server.js
@@ -4,16 +4,23 @@ const server = express();
 //configurar pasta publica
 server.use(express.static('public'));
 
+//utilizando template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true,
+});
+
 //configurar caminhos da minha aplicação
 //página inicial
 //req=requisição="pedido"
 //res=resposta
 server.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  return res.render('index.html', { title: 'Um título' });
 });
 
 server.get('/create-point', (req, res) => {
-  res.sendFile(__dirname + '/views/create-point.html');
+  return res.render('create-point.html');
 });
 //ligar o servidor
 server.listen(3000);
