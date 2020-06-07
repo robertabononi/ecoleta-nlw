1ea20dc5ab191e43caaffa90af61e01d58077124
Roberta Bononi da Silva <robertabononi@users.noreply.github.com>
2020-06-07 17:07:38 -0300

    correcao bugs

--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const server = express();
 
 //pegar o banco de dados
-const db = require('./database/db.js');
+const db = require('./database/db');
 
 //configurar pasta publica
 server.use(express.static('public'));
+
+//habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({ extended: true }));
 
 //utilizando template engine
 const nunjucks = require('nunjucks');
@@ -23,7 +26,49 @@ server.get('/', (req, res) => {
 });
 
 server.get('/create-point', (req, res) => {
+  //req.query: Query Strings da nossa url
+  //console.log(req.query);
+
   return res.render('create-point.html');
+});
+
+server.post('/savepoint', (req, red) => {
+  //req.body: o corpo do formulário
+  // console.log(req.body);
+
+  //inserir dados no banco de dados
+
+  const query = `
+      INSERT INTO places (
+        image,
+        name,
+        address,
+        address2,
+        state,
+        city,
+        items
+      ) VALUES (?,?,?,?,?,?,?);
+    `;
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items,
+  ];
+  function afterInsertData(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Cadastrado com sucesso');
+    console.log(this);
+
+    return res.send('ok');
+  }
+
+  db.run(query, values, afterInsertData);
 });
 
 server.get('/search', (req, res) => {
